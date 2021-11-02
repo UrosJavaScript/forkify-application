2c41f89fee4a128c9f7bdd8f28c481458930a970
kowcicuros70@gmail.com <kowcicuros70@gmail.com>
2021-11-02 21:40:37 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b751914b-76f6-4cbe-b6a6-6575f65e3177';
+export const MODAL_CLOSE_SEC = 2.5;
